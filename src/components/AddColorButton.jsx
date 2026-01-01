3db6c9c6e393c099094c5333490d06006ddec4ff
marwaneTRK <marwantraiki@gmail.com>
2026-01-01 23:57:58 +0100

    fixing tailwind class

--- src/components/AddColorButton.jsx
+++ src/components/AddColorButton.jsx
@@ -8,9 +8,9 @@ const AddColorButton = () => {
   }
   return (
     <div className="group relative inline-flex justify-center items-center">
-      <div className="absolute inset-0 rounded-full bg-gradient-to-r from-pink-500 via-purple-500 to-cyan-500 blur-lg opacity-40 group-hover:opacity-70 transition-opacity duration-300"></div>
+      <div className="absolute inset-0 rounded-full bg-linear-to-r from-pink-500 via-purple-500 to-cyan-500 blur-lg opacity-40 group-hover:opacity-70 transition-opacity duration-300"></div>
 
-      <div className="relative inline-block p-[3px] rounded-full bg-gradient-to-r from-pink-500 via-purple-500 to-cyan-500 transition-transform duration-300 group-hover:scale-105 group-active:scale-95 shadow-xl">
+      <div className="relative inline-block p-0.75 rounded-full bg-linear-to-r from-pink-500 via-purple-500 to-cyan-500 transition-transform duration-300 group-hover:scale-105 group-active:scale-95 shadow-xl">
         <button
           className="flex items-center gap-3 md:gap-4 px-8 md:px-10 py-3 md:py-4 rounded-full bg-[#1e1b28] group-hover:bg-[#2a2635] transition-colors cursor-pointer w-full h-full"
           onClick={handleLogin}
